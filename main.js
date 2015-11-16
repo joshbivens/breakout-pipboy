3c7ed8a76946a2d65bb8b97b680eee150988fde2
Josh Bivens <hotbioform@gmail.com>
2015-11-16 13:42:52 -0700

    Add electron

---
+++ main.js
@@ -0,0 +1,20 @@
+var app = require("app");
+var browserWindow = require("browser-window");
+
+require("crash-reporter").start();
+
+var mainWindow = null;
+
+app.on("window-all-closed", function() {
+  if(process.platform != "darwin") {
+    app.quit();
+  }
+});
+
+app.on("ready", function() {
+  mainWindow = new browserWindow({width: 815, height: 615});
+  mainWindow.loadUrl("file://" + __dirname + "/index.html");
+  mainWindow.on("closed", function() {
+    mainWindow = null;
+  });
+});
