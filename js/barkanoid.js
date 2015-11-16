13eb03c2ff81be0b8fe2f0854ba83ffb2aa62113
Josh Bivens <hotbioform@gmail.com>
2015-11-16 13:43:27 -0700

    Change file paths

--- js/barkanoid.js
+++ js/barkanoid.js
@@ -29,15 +29,15 @@ var textLarge = {
 };
 
 function phaserPreload() {
-  game.load.image("background", "/assets/background.jpg");
-  game.load.image("tile0", "/assets/tile0.png");
-  game.load.image("tile1", "/assets/tile1.png");
-  game.load.image("tile2", "/assets/tile2.png");
-  game.load.image("tile3", "/assets/tile3.png");
-  game.load.image("tile4", "/assets/tile4.png");
-  game.load.image("tile5", "/assets/tile5.png");
-  game.load.image("paddle", "/assets/paddle.png");
-  game.load.image("ball", "/assets/ball.png");
+  game.load.image("background", "assets/background.jpg");
+  game.load.image("tile0", "assets/tile0.png");
+  game.load.image("tile1", "assets/tile1.png");
+  game.load.image("tile2", "assets/tile2.png");
+  game.load.image("tile3", "assets/tile3.png");
+  game.load.image("tile4", "assets/tile4.png");
+  game.load.image("tile5", "assets/tile5.png");
+  game.load.image("paddle", "assets/paddle.png");
+  game.load.image("ball", "assets/ball.png");
 }
 
 function phaserCreate() {
