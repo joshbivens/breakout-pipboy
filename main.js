8c5b52b065b8b214863f5751f7090db09b56c378
Josh Bivens <hotbioform@gmail.com>
2015-11-16 14:06:29 -0700

    Change width, add center option

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ app.on("window-all-closed", function() {
 });
 
 app.on("ready", function() {
-  mainWindow = new browserWindow({width: 815, height: 615});
+  mainWindow = new browserWindow({width: 850, center: true});
   mainWindow.loadURL("file://" + __dirname + "/index.html");
   mainWindow.on("closed", function() {
     mainWindow = null;
